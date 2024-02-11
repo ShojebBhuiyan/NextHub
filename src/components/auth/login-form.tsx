e134f3bbfc7336741709670ac33c4bf044386570
Md. Shahriar Islam Bhuiyan <shojeb.bhuiyan@gmail.com>
2024-02-11 16:51:47 +0600

    Form error and success components

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -14,6 +14,7 @@ import {
 } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import FormError from "../form-error";
 
 export function LoginForm() {
   const form = useForm<z.infer<typeof LoginSchema>>({
@@ -24,9 +25,11 @@ export function LoginForm() {
     },
   });
 
+  function onSubmit(values: z.infer<typeof LoginSchema>) {}
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
         <div className="space-y-4">
           <FormField
             control={form.control}
@@ -59,6 +62,7 @@ export function LoginForm() {
             )}
           />
         </div>
+        <FormError message="Invalid credentials!" />
         <Button type="submit" className="w-full">
           Login
         </Button>
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,7 +2,9 @@ import { z } from "zod";
 
 export const LoginSchema = z.object({
   email: z.string().email(),
-  password: z.string(),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
 });
 
 export const RegisterSchema = z.object({
